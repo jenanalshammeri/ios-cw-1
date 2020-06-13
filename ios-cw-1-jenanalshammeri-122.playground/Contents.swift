63ce7ba3f72ab951e6b17070f47912d1921970d7
jenan alshammeri <jenan.alshammeri1@gmail.com>
2020-06-13 18:55:44 +0300

    donee

---
+++ ios-cw-1-jenanalshammeri-122.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let name = "Jenan Alshammeri"
+var hobby1 = " painting"
+var hobby2 = " writing"
+var age = 15
+var height = 168
+print(" Hi my name is \(name) and i like \(hobby1) and \(hobby2)")
+print("Im \(age) and \(height) cm tall")
