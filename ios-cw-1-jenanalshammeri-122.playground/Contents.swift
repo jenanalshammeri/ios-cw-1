36031c92c7ee465d1d4313ddf459cfd668e08d27
jenan alshammeri <jenan.alshammeri1@gmail.com>
2020-06-13 20:07:23 +0300

    done with part 2

--- ios-cw-1-jenanalshammeri-122.playground/Contents.swift
+++ ios-cw-1-jenanalshammeri-122.playground/Contents.swift
@@ -2,8 +2,23 @@ import UIKit
 
 let name = "Jenan Alshammeri"
 var hobby1 = " painting"
-var hobby2 = " writing"
+var hobby2 = " cooking "
 var age = 15
 var height = 168
 print(" Hi my name is \(name) and i like \(hobby1) and \(hobby2)")
 print("Im \(age) and \(height) cm tall")
+
+var friendName = " Munera Altunaib"
+var fiendHobby1 = " reading "
+var friendHobby2 = " baking "
+var friendAge = 16
+var friendHeight = 1.5
+
+var ageDifference = age - friendAge
+var ageCompare = 15 < 16
+var nameDifference = name != friendName
+var hobby1Difference = hobby1 != friendHobby2
+var hobby2Difference = hobby2 != friendHobby2
+var heightDiffernce = 168 > 1.5
+
+// i cant put hobby1 and friendHobby1 مادري ليش معلق it doesnt recognize friendHobby1 as a variable
